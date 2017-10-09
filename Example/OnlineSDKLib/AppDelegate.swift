efd6c10bbb971107e6e926a868d39e587bded549
Manjunath-Ramesh <manjunath.ramesh@onepointglobal.com>
2017-10-09 16:12:12 +0530

    Live SDK commit

--- Example/OnlineSDKLib/AppDelegate.swift
+++ Example/OnlineSDKLib/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        OPGSDK.initialize(withUserName: "", withSDKKey: "")
-        OPGSDK.setAppVersion("")
+        OPGSDK.initialize(withUserName: "SDK_USername", withSDKKey: "SDK_Key")
+        OPGSDK.setAppVersion("APP_VERSION")
         return true
     }
 
